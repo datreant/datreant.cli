8e00e619cc775c18b1648936ec27fcd7a03e17d5
Max Linke <maxlinke88@gmail.com>
2018-07-28 21:21:13 +0200

    black setup.py

--- setup.py
+++ setup.py
@@ -8,33 +8,30 @@ For a basic installation just type the command::
 """
 
 from setuptools import setup, find_packages
+
 print(find_packages())
 
-setup(name='datreant.cli',
-      version='0.7.0-dev',
-      description='convenient CLI interface for Treant directories',
-      author='Max Linke',
-      author_email='max_linke@gmx.de',
-      url = 'http://datreant.org/',
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
-      packages=find_packages(),
-      scripts=[],
-      license='BSD',
-      long_description=open('README.rst').read(),
-      install_requires=[
-          'datreant',
-          'six',
-          'click',
-          ],
-      entry_points={'console_scripts':
-          ['dtr=datreant.cli.cli:cli']}
-      )
+setup(
+    name="datreant.cli",
+    version="0.7.0-dev",
+    description="convenient CLI interface for Treant directories",
+    author="Max Linke",
+    author_email="max_linke@gmx.de",
+    url="http://datreant.org/",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=find_packages(),
+    scripts=[],
+    license="BSD",
+    long_description=open("README.rst").read(),
+    install_requires=["datreant", "six", "click"],
+    entry_points={"console_scripts": ["dtr=datreant.cli.cli:cli"]},
+)
